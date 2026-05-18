0772b99776d81ef595266ced209a9be9302376d6
juanjmir <73290977+juanjmir@users.noreply.github.com>
2026-05-18 12:41:51 -0400

    .

--- InformeNovedades_bo/app_todos.js
+++ InformeNovedades_bo/app_todos.js
@@ -85,9 +85,18 @@ function renderTabla(novedades) {
                 horarioMostrar = horarioRaw.toString().trim().substring(0, 5);
             }
         }
-        
+
         let fechaMostrar = "N/A";
-        let fechaRaw = getData('Fecha') || getData('Fecha informe'); // Captura cualquiera de los dos nombres
+        let fechaRaw = getData('Fecha informe'); 
+        if (fechaRaw) {
+            // Separa el texto en la 'T' para eliminar "T04:00:00.000Z" y quedarse con "2026-05-19"
+            const fechaSolo = fechaRaw.toString().includes('T') ? fechaRaw.split('T')[0] : fechaRaw;
+            // Da vuelta el orden: de YYYY-MM-DD a DD-MM-YYYY
+            fechaMostrar = fechaSolo.trim().split('-').reverse().join('-');
+        }   
+
+        let fechaMostrar = "N/A";
+        let fechaRaw = getData('Fecha termino'); 
         if (fechaRaw) {
             // Separa el texto en la 'T' para eliminar "T04:00:00.000Z" y quedarse con "2026-05-19"
             const fechaSolo = fechaRaw.toString().includes('T') ? fechaRaw.split('T')[0] : fechaRaw;
